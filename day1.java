a54563d6ae645124a3683f644484f03c3c66eefc
riteshyadav31 <dev.ritesh.in@gmail.com>
2025-12-28 20:41:12 +0530

    reversed string program made

--- day1.java
+++ day1.java
@@ -4,15 +4,24 @@ class day1{
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the String:");
         String s=sc.nextLine();
-        String s1=new String("raju");// created in heap memory
-        String s2=new String("rahul");// created in heap memory
-        String s3=new String("raju");// created in heap memory
-        String s4="raju";// created in string pool
-        System.out.println(s1==s); // compares memory locations, false
-        System.out.println(s1==s3); // compares memory locations, false
-        System.out.println(s1.equals(s3)); // true, compares content
-        System.out.println(s1.equals(s4)); // true, compares content
-        System.out.println("You entered: " + s);
+        // String s1=new String("raju");// created in heap memory
+        // String s2=new String("rahul");// created in heap memory
+        // String s3=new String("raju");// created in heap memory
+        // String s4="raju";// created in string pool
+        // System.out.println(s1==s); // compares memory locations, false
+        // System.out.println(s1==s3); // compares memory locations, false
+        // System.out.println(s1.equals(s3)); // true, compares content
+        // System.out.println(s1.equals(s4)); // true, compares content
+         System.out.println("You entered: " + s);
+int length = s.length();
+String reversed = "";
+for (int i=length-1; i>=0; i--) {
+reversed+=s.charAt(i);
+}
+        System.out.println("reverse of the string: " + reversed);
+
+
+
         sc.close();
     }
 }

d9de3fa12729ae1f9a17ef8abb272ce40aae3897
riteshyadav31 <dev.ritesh.in@gmail.com>
2025-12-28 20:09:12 +0530

    understanding of string pool constant and memory and difference between == and .equal() operator

---
+++ day1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class day1{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the String:");
+        String s=sc.nextLine();
+        String s1=new String("raju");// created in heap memory
+        String s2=new String("rahul");// created in heap memory
+        String s3=new String("raju");// created in heap memory
+        String s4="raju";// created in string pool
+        System.out.println(s1==s); // compares memory locations, false
+        System.out.println(s1==s3); // compares memory locations, false
+        System.out.println(s1.equals(s3)); // true, compares content
+        System.out.println(s1.equals(s4)); // true, compares content
+        System.out.println("You entered: " + s);
+        sc.close();
+    }
+}

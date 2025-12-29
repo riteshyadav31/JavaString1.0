8b9d20c07cf138afea07e19397a57b5e21ee82b4
riteshyadav31 <dev.ritesh.in@gmail.com>
2025-12-29 20:22:12 +0530

    understand the string final keyword means and why string class is final because string  class should not be modified so any class should not inherit the string and string properties should be unchanged

--- day2.java
+++ day2.java
@@ -1,11 +1,18 @@
 public class day2 {
     public static void main(String[] args) {
-      String s1 = "hello";
-      String s2 = "hello"; // s2 refers to the same string in the string pool as s1
-      System.out.println(s1 == s2); // true, same reference in string pool
-      s1=s1.concat(s2); // creates a new string in the string pool
-System.out.println(s1 == s2); // false, different references
-         System.out.println(s1.concat(" world "));
-        System.out.println(s1); 
+      final String s1 = "hello";
+      String s3 = s1.concat(" world"); // here s1 remains unchanged
+      System.out.println(s3);
+      s3 = s3.concat("!!!"); // here s3 is reassigned to a new string
+      System.out.println(s3);
+ //   s1=s1.concat(" world"); // error: cannot assign a value to final variable s1
+      System.out.println(s1);
+//       String s2 = "hello"; // s2 refers to the same string in the string pool as s1
+//       System.out.println(s1 == s2); // true, same reference in string pool
+//       s1=s1.concat(s2); // creates a new string in the string pool
+// System.out.println(s1 == s2); // false, different references
+//          System.out.println(s1.concat(" world "));
+//         System.out.println(s1);
     }}
 
+ 

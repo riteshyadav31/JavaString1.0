764b515ccf2433cd540c66f0df99fd7a33403916
riteshyadav31 <dev.ritesh.in@gmail.com>
2025-12-31 20:27:20 +0530

    understand the working of trim() method

--- day4.java
+++ day4.java
@@ -3,26 +3,35 @@
 class day4{
     public static void main(String[] args) {
  
-String s1 = "hello";
+// String s1 = "hello";
 
-int len = s1.length();// calculates length of the string it counts number of characters in the string
-System.out.println("Length of the string: " + len);
-// // String s2=null ;
-// // int len2 = s2.length(); // This will throw NullPointerException
-// System.out.println("Length of the string: " + len2);
-String s2 = "";
-int len2 = s2.length(); // Length is 0 for empty string
-System.out.println("Length of the empty string: " + len2);
+// int len = s1.length();// calculates length of the string it counts number of characters in the string
+// System.out.println("Length of the string: " + len);
+// // // String s2=null ;
+// // // int len2 = s2.length(); // This will throw NullPointerException
+// // System.out.println("Length of the string: " + len2);
+// String s2 = "";
+// int len2 = s2.length(); // Length is 0 for empty string
+// System.out.println("Length of the empty string: " + len2);
 
-String s3 = "Hello World";
-System.out.println("this is empty string: " + s3.isEmpty()); // false, string is not empty
-String s4 = "";
-System.out.println("this is empty string: " + s4.isEmpty()); // true, string is empty
-String s5 = "Hello World";
-System.out.println("this is blank string: " + s5.isBlank()); // false, string is not blank
-String s6 = "   ";  
-System.out.println("this is blank string: " + s6.isBlank()); // true, string is blank (only whitespace)
-String s7 = "empty";
-System.out.println(s7.isEmpty()); // false, string is not empty
+// String s3 = "Hello World";
+// System.out.println("this is empty string: " + s3.isEmpty()); // false, string is not empty
+// String s4 = "";
+// System.out.println("this is empty string: " + s4.isEmpty()); // true, string is empty
+// String s5 = "Hello World";
+// System.out.println("this is blank string: " + s5.isBlank()); // false, string is not blank
+// String s6 = "   ";  
+// System.out.println("this is blank string: " + s6.isBlank()); // true, string is blank (only whitespace)
+// String s7 = "empty";
+// System.out.println(s7.isEmpty()); // false, string is not empty
+
+String s8 = "   rahul   ";
+
+System.out.println("String s8: " + s8);
+System.out.println("String s8 after trim: " + s8.trim());
+String s9="   hello    world   ";
+System.out.println("String s9: " + s9);
+System.out.println("String s9 after trim: " + s9.trim());
+
     }
 }

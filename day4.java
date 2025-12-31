147e9188c8a33f06a59eafb74803b69877dbbc3d
riteshyadav31 <dev.ritesh.in@gmail.com>
2025-12-31 20:04:56 +0530

    length() method in String working and its exception in the null pointer

---
+++ day4.java
@@ -0,0 +1,16 @@
+class day4{
+    public static void main(String[] args) {
+ 
+String s1 = "hello";
+
+int len = s1.length();
+System.out.println("Length of the string: " + len);
+// // String s2=null ;
+// // int len2 = s2.length(); // This will throw NullPointerException
+// System.out.println("Length of the string: " + len2);
+String s2 = "";
+int len2 = s2.length(); // Length is 0 for empty string
+System.out.println("Length of the empty string: " + len2);
+
+    }
+}

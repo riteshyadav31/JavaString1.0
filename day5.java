0443819ce611935dd15852b52856254cf9988b39
riteshyadav31 <dev.ritesh.in@gmail.com>
2026-01-01 18:39:45 +0530

    equals() method and equalsIgnoreCase() method understanding

---
+++ day5.java
@@ -0,0 +1,16 @@
+class day5{
+    public static void main(String[] args) {
+
+        String s1 = "hello world";
+        String s2 = "HELLO WORLD";
+        System.out.println("s1 equals s2: " + s1.equals(s2)); // false, case-sensitive comparison
+        System.out.println("s1 equalsIgnoreCase s2: " + s1.equalsIgnoreCase(s2)); // true, case-insensitive comparison
+        
+
+
+
+
+
+
+    }
+}

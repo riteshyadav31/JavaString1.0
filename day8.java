bc63e75e46b4bba17b3bd4d5d630623f9c9d34b5
riteshyadav31 <dev.ritesh.in@gmail.com>
2026-01-04 19:01:09 +0530

    understand the searching related methods in java like charAt(), indexOf() lastIndexOf() contains() startsWith() endsWith()

---
+++ day8.java
@@ -0,0 +1,10 @@
+public class day8 {
+    public static void main(String args[]){
+        String s1="  Hello  World  "; 
+        System.out.println(s1.charAt(4)); 
+        System.out.println(s1.indexOf("o"));
+        System.out.println(s1.lastIndexOf("o"));
+        System.out.println(s1.contains("World"));
+        System.out.println(s1.startsWith("  He"));
+        System.out.println(s1.endsWith("  "));
+    }}
